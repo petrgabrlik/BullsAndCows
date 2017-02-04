c4d70946d0d63548ce18d3cac4bd99461c97f640
Petr Gabrlik <petrgabrlik@email.cz>
2017-02-04 16:36:47 +0100

    Added saving results to file
    Added basic functionality of saving results to a file (function
    savingresults). Player must input his/her name and results are printed.

--- bullsandcows.py
+++ bullsandcows.py
@@ -17,15 +17,19 @@ Let's play a bulls and cows game.""")
     while True:
         # Game loop
         rann = rngen(dn)
-        print("I've generated a random {:}-digit number for you.\nGuess that number".format(dn)) 
-       
-        guess = 1	
+        #print("(Tajne cislo: " + str(rann) + ")")
+        print("I've generated a random {:}-digit number for you.\nGuess that number".format(dn))
+
+        guess = 1
         while True:
             # Guess loop
             usrn = usrinput(dn)
             if rann == usrn:
                 print("Correct, you've guessed the right number in {:} {:}!".format(guess, 'guess' if guess == 1 else 'guesses'))
                 print("That's {:}.".format(evaluation(guess)))
+                name = input("Enter your name: ")
+                saveresults(name, guess)
+                print(open("results.txt", "r").read())
                 break
             else:
                 guess += 1
@@ -34,13 +38,13 @@ Let's play a bulls and cows game.""")
                     if str(usrn)[num] == str(rann)[num]: bulls += 1
                     elif str(usrn)[num] in str(rann): cows += 1
                 print('{:} {:}, {:} {:}'.format(bulls, 'bull' if bulls == 1 else 'bulls', cows, 'cow' if cows == 1 else 'cows'))
-    
+
         # Play again?
         again = ''
         while not (again == 'Y' or again == 'N'):
             again = input('Do you want to play again? (Y/N) ').upper()
-        if again == 'N': 
-            print('Thank you for playing! Bye bye.')            
+        if again == 'N':
+            print('Thank you for playing! Bye bye.')
             break
 
 
@@ -48,14 +52,14 @@ def rngen(n):
     # Generates n-digit integer, without repeating numbers
     rn = []
     while len(rn) < n:
-        newnum = ( str(random.randint(0,9)) ) 
+        newnum = ( str(random.randint(0,9)) )
         if newnum not in rn and not (len(rn) == 0 and int(newnum) == 0): rn.append(newnum)
     return int(''.join(rn))
 
-    
+
 def usrinput(n):
     # Reads user's 4-digit integer, repeating numbers is not allowed
-    while True:    
+    while True:
         try:
             usrn = abs(int(input()))
             if len(str(usrn)) > n: print('Too long. Please enter {:}-digit integer.'.format(n))
@@ -70,7 +74,9 @@ def evaluation(n):
     # Evaluates results
     return {1<=n<4: 'amazing', 4<=n<10: 'really good', 10<=n<20: 'average', 20<=n: 'not so good'}[1]
 
+def saveresults(name, guess):
+    # Saves results to file
+    print(name, guess, sep="\t", file=open("results.txt", "a"))
 
 if __name__ == "__main__":
-    main()        
-
+    main()
